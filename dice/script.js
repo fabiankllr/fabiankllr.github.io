cf3b62aefd5e71b17ff25eb368ead5c99c01dcfc
Fabian Keller <fabiankllr04@gmail.com>
2023-12-30 19:32:43 +0100

    clean up js code for dice

--- dice/script.js
+++ dice/script.js
@@ -1,9 +1,12 @@
-let numberOfSides = 6;
+let numberOfSides = 6; // initially set to standard dice with 6 sides
+
+const NUMBER_OUTPUT = document.getElementById("number");
+const NUMBER_OF_SIDES_INPUT = document.getElementById("numberOfSidesInput");
 
 document.getElementById("dice").addEventListener("click", rollDice);
 
 function rollDice() {
-  document.getElementById("number").textContent = getRandomNumberWithinBoundaries();
+  NUMBER_OUTPUT.textContent = getRandomNumberWithinBoundaries();
 }
 
 function getRandomNumberWithinBoundaries() {
@@ -11,14 +14,14 @@ function getRandomNumberWithinBoundaries() {
 }
   
 function setDiceNumber() {
-  let number = document.getElementById("numberOfSidesInput").value;
+  const NUMBER_GIVEN = NUMBER_OF_SIDES_INPUT.value;
 
-  if (number < 2 || number > 100) {
+  if (NUMBER_GIVEN < 2 || NUMBER_GIVEN > 100) {
     alert("Number of sides must be between 2 and 100");
-    document.getElementById("numberOfSidesInput").value = numberOfSides;
-    
+    NUMBER_OF_SIDES_INPUT.value = numberOfSides;
+
     return;
   }
 
-  numberOfSides = number;
+  numberOfSides = NUMBER_GIVEN;
 }
